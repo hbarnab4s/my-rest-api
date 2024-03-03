e379dc921075f5bbd9fc52a125ffbae6aadc59f7
Hites Barnabás <86848934+hbarnab4s@users.noreply.github.com>
2024-03-03 20:50:55 +0100

    set database

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => {
+    console.error(error)
+})
+
+db.once('open', () => {
+    console.log('Connected to database')
 })
 
 app.listen(port, () => {
